9318e6d9519c94217033e55a4542c26a293eea86
John Nelson <automationaddict@me.com>
2024-02-24 16:37:52 -0500

    Setting up for python3 installation
    More work to do on the installer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use colored::Colorize;
 use core::panic;
 use sysinfo::System;
+use which::which;
 use whoami::Platform;
 
 fn main() {
@@ -34,12 +35,32 @@ fn main() {
     // get the package for this ditribution
     let dist = match whoami::devicename_os().into_string() {
         Ok(res) => res.to_lowercase().replace(' ', "_"),
-        Err(err) => panic!("Cannot get distribution name {:?}", err),
+        Err(err) => panic!(
+            "{} {:?}",
+            "Cannot get distribution name".red(),
+            err.into_string().unwrap().red()
+        ),
     };
 
+    //  limit this to a single distribution for the moment
     if dist != "pop_os" {
-        panic!("Tooling is not configured for this OS yet")
+        panic!(
+            "{}",
+            "Tooling is not configured for this distribution yet".red()
+        )
     }
 
     // check if python3 is installed
+    match which("python3") {
+        Ok(path) => println!(
+            "{} {}",
+            "Python available at:".green(),
+            path.into_os_string().into_string().unwrap().green()
+        ),
+        Err(err) => println!(
+            "{} {}",
+            "Installing Python3".yellow(),
+            err.to_string().yellow()
+        ),
+    };
 }
