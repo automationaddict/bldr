1bce620f6534bb1e46fa64f6dcc089a510a68632
John Nelson <automationaddict@me.com>
2024-02-24 14:18:40 -0500

    Get users name, host OS, and check if tool will be supported

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use colored::Colorize;
+use core::panic;
+use sysinfo::System;
+use whoami::Platform;
+
 fn main() {
-    println!("Hello, world!");
+    // get users real name
+    println!("{} {}", "Hello".green(), whoami::realname().green());
+
+    let mut sys = System::new_all();
+
+    sys.refresh_all();
+
+    // get OS of the system
+    match whoami::platform() {
+        Platform::Linux => println!(
+            "{} {} {} {}",
+            "You are running Distro:".green(),
+            whoami::distro().green(),
+            "on Arch:".green(),
+            whoami::arch().to_string().green()
+        ),
+        Platform::Unknown(s) => eprintln!("Unknown OS {}", s.red()),
+        _ => panic!("{}", "Cannot identify the OS".red()),
+    }
+
+    //check if this tool is supported
+    if sysinfo::IS_SUPPORTED_SYSTEM {
+        println!("{}", "This OS is suported by this tool".green())
+    } else {
+        panic!("{}", "This OS is not supported by this tool".red());
+    }
 }
